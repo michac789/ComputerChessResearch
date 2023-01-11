05ee391ba8b9ac778f31c9aa9aa40a128fda4f7a
MichaelAndrew Chan <michaelac978@gmail.com>
2023-01-11 21:04:15 +0800

    [Phase1] basic game loop and move creation

--- phase1/chess.py
+++ phase1/chess.py
@@ -1,4 +1,3 @@
-from typing import Tuple
 from chess_pieces import Piece, King, Queen, Rook, Knight, Bishop, Pawn
 from constants import PIECES
 
@@ -6,8 +5,9 @@ from constants import PIECES
 class ChessBoard:
     def __init__(self):
         self.board = [[PIECES['EMPTY_TILE'] for _ in range(8)] for _ in range(8)]
+        self.player = 0
     
-    def place_pieces(self, pieces: list[Tuple[Piece, int, int, int]]):
+    def _place_pieces(self, pieces: list[tuple[Piece, int, int, int]]):
         for piece in pieces:
             Class, player, i, j = piece
             self.board[i][j] = Class(player, i, j)
@@ -16,7 +16,7 @@ class ChessBoard:
         for p in range(2):
             row = 7 - 7 * p
             row2 = 6 - 5 * p
-            self.place_pieces([
+            self._place_pieces([
                 (Rook, p, row, 0),
                 (Knight, p, row, 1),
                 (Bishop, p, row, 2),
@@ -35,13 +35,42 @@ class ChessBoard:
             print('')
         print('')
 
+    def _get_coordinate(self, msg: str='Enter tile: ', allow_exit: bool=False) -> tuple[int] | None:
+        while True:
+            tile = input(msg)
+            if allow_exit and tile == '-1': return None, None
+            if len(tile) == 2:
+                try: tile_x = int(tile[1])
+                except ValueError: tile_x = -1
+                if 65 <= ord(tile[0]) <= 72 and 1 <= tile_x <= 8:
+                    break
+            print('Invalid tile entered, try again!')
+        return (8 - tile_x, ord(tile[0]) - 65)
+
+    def move_piece(self):
+        print(f"It is {('white' if self.player == 0 else 'black')}'s turn to move.")
+        while True:
+            y, x = chess._get_coordinate(msg='Enter your piece tile to move: ')
+            if self.board[y][x] != PIECES['EMPTY_TILE'] and self.board[y][x].player == self.player:
+                while True:
+                    valid_moves = self.board[y][x].get_valid_moves(self.player, self.board)
+                    print(f'valid moves: {valid_moves}')
+                    print(f'valid moves: {"".join(chr(move[1] + 65) + str(8 - move[0]) + " " for move in valid_moves)}')
+                    p, q = chess._get_coordinate(msg='Enter valid tile to move this piece: (type -1 to cancel) ', allow_exit=True)
+                    if not p: break
+                    if (p, q) in valid_moves:
+                        chess.board[p][q] = chess.board[y][x]
+                        chess.board[y][x] = PIECES['EMPTY_TILE']
+                        self.player = (self.player + 1) % 2
+                        return
+                    print('Invalid tile selected, try again!')
+            print('Invalid piece selected, try again!')
+
+
 if __name__ == "__main__":
     chess = ChessBoard()
     chess.initialize_board()
-    chess.display_board()
-    chess.board[4][1] = Queen(0, 4, 1)
-    a = chess.board[4][1].get_valid_moves(0, chess.board)
-    print(a)
-    chess.display_board()
-    print("Hello World ♟")
-    print('\u25FB \u265F')
+
+    while True:
+        chess.display_board()
+        chess.move_piece()
